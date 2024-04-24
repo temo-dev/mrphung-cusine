cdf56153c8d7f953a14aaf2ef94867961894110e
TuanAnh-Seta <anhnt6879@seta-international.vn>
2024-04-24 12:05:19 +0200

    fix-header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -4,161 +4,140 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { gsap, Power3, Power2, Expo } from "gsap";
+import React, { useRef, useEffect } from "react";
+import { gsap, Power3, Power2, Expo, TweenTarget } from "gsap";
 
 function Header({ data }) {
-  if (typeof window !== "undefined") {
-    /*--------------------------------------------------------------
-     1. Heder Border Animations
- --------------------------------------------------------------*/
-    const border = document.querySelectorAll(".nav-bar-border");
-    if (border) {
-      gsap.fromTo(
-        border,
-        {
-          duration: 1,
-          width: "0%",
-        },
-        {
-          delay: 1,
-          duration: 1,
-          width: "100%",
-        }
-      );
-    }
-    const headerLogo = document.querySelectorAll(".header-logo");
-    if (headerLogo) {
-      gsap.fromTo(
-        headerLogo,
-        {
-          duration: 1,
-          height: "0%",
-        },
-        {
-          delay: 0.5,
-          duration: 1,
-          height: "100%",
-        }
-      );
-    }
-    const menuToggle = document.querySelectorAll(".ak-menu-toggle");
-    if (menuToggle) {
-      gsap.fromTo(
-        menuToggle,
-        {
-          duration: 1,
-          height: "0%",
-        },
-        {
-          duration: 1,
-          height: "100%",
-        }
-      );
-    }
-    /*--------------------------------------------------------------
-      2. Top Header Slider Bar
- --------------------------------------------------------------*/
-    const isSite = document.querySelectorAll(".ak-site_header");
-    if (isSite) {
-      let akMenuToggle = document.getElementById("akMenuToggle");
-      const hamsdf = document.querySelector(".ak-main_header_right");
-      const menu = document.querySelector(".top-main-menu");
-      const links = menu?.querySelectorAll(".top-main-menu-li");
+  let akMenuToggle = useRef(null);
+  let hamsdf = useRef(null);
+  let menuRef = useRef(null);
+  let border1 = useRef(null);
 
-      const menuItemBar = gsap.timeline();
-      var navtl = gsap.timeline({ paused: true });
-      navtl.set(hamsdf, {
-        opacity: 0,
-        transition: "all 0.5s ease-in",
-      });
+  useEffect(() => {
+    const menu = menuRef.current;
+    const links = menuRef.current.children;
+    const menuItemBar = gsap.timeline();
+    var navtl = gsap.timeline({ paused: true });
+    navtl.set(hamsdf, {
+      opacity: 0,
+      transition: "all 0.5s ease-in",
+    });
+    menuItemBar.to(
+      ".bar-1",
+      0.5,
+      {
+        attr: { d: "M20,0 L2,9" },
+        x: 1,
+        ease: Power2.easeInOut,
+      },
+      "start"
+    );
 
-      menuItemBar.to(
-        ".bar-1",
-        0.5,
-        {
-          attr: { d: "M20,0 L2,9" },
-          x: 1,
-          ease: Power2.easeInOut,
-        },
-        "start"
-      );
+    menuItemBar.to(
+      ".bar-2",
+      0.5,
+      {
+        autoAlpha: 0,
+      },
+      "start"
+    );
+    menuItemBar.to(
+      ".bar-3",
+      0.5,
+      {
+        attr: { d: "M22,12 L2,0" },
+        x: 1,
+        ease: Power2.easeInOut,
+      },
+      "start"
+    );
 
-      menuItemBar.to(
-        ".bar-2",
-        0.5,
-        {
-          autoAlpha: 0,
-        },
-        "start"
-      );
-      menuItemBar.to(
-        ".bar-3",
-        0.5,
-        {
-          attr: { d: "M22,12 L2,0" },
-          x: 1,
-          ease: Power2.easeInOut,
-        },
-        "start"
-      );
+    gsap.fromTo(
+      akMenuToggle,
+      {
+        duration: 1,
+        height: "0%",
+      },
+      {
+        duration: 1,
+        height: "100%",
+      }
+    );
 
-      navtl
-        .to(menu, {
+    navtl
+      .to(menu, {
+        duration: 0.3,
+        opacity: 1,
+        height: "116vh",
+        ease: Expo.easeInOut,
+        stagger: 0.1,
+        background: "#040D10",
+      })
+      .fromTo(
+        links,
+        {
+          duration: 0.3,
+          opacity: 0,
+          y: 30,
+          ease: Expo.easeInOut,
+        },
+        {
           duration: 0.3,
           opacity: 1,
-          height: "116vh",
-          ease: Expo.easeInOut,
+          y: -20,
           stagger: 0.1,
-          background: "#040D10",
-        })
-        .fromTo(
-          links,
-          {
-            duration: 0.3,
-            opacity: 0,
-            y: 30,
-            ease: Expo.easeInOut,
-          },
-          {
-            duration: 0.3,
-            opacity: 1,
-            y: -20,
-            stagger: 0.1,
-            ease: Expo.easeInOut,
-          }
-        );
-
-      navtl.reverse();
-      menuItemBar.reverse();
-      gsap.set(links, {
-        display: "none",
-      });
-
-      akMenuToggle?.addEventListener("click", () => {
-        if (!navtl.reversed()) {
-          gsap.set(links, {
-            padding: "0%",
-            display: "block",
-            paddingLeft: "0%",
-            paddingRight: "0%",
-          });
-          navtl.set(hamsdf, {
-            opacity: 1,
-            transition: "all 0.5s ease-in",
-          });
-        } else {
-          gsap.set(links, {
-            display: "flex",
-            paddingLeft: "15%",
-            paddingRight: "5%",
-          });
+          ease: Expo.easeInOut,
         }
-        navtl.reversed(!navtl.reversed());
-        menuItemBar.reversed(!menuItemBar.reversed());
-      });
-    }
-  }
+      );
+
+    navtl.reverse();
+    menuItemBar.reverse();
+
+    akMenuToggle.addEventListener("click", () => {
+      if (!navtl.reversed()) {
+        gsap.set(links, {
+          padding: "0%",
+          display: "block",
+          paddingLeft: "0%",
+          paddingRight: "0%",
+        });
+        gsap.set(menu, {
+          opacity: 1,
+          transition: "all 0.5s ease-in",
+        });
+      } else {
+        gsap.set(links, {
+          display: "flex",
+          paddingLeft: "15%",
+          paddingRight: "5%",
+          opacity: 1,
+        });
+        gsap.set(menu, {
+          opacity: 0,
+          transition: "all 0.5s ease-in",
+        });
+      }
+      navtl.reversed(!navtl.reversed());
+      menuItemBar.reversed(!menuItemBar.reversed());
+    });
+
+    /*--------------------------------------------------------------
+  //      1. Heder Border Animations
+  //  --------------------------------------------------------------*/
+    gsap.fromTo(
+      border1,
+      {
+        duration: 1,
+        width: "0%",
+      },
+      {
+        delay: 1,
+        duration: 1,
+        width: "100%",
+      }
+    );
+  }, []);
+
   return (
     <header className="ak-site_header ak-style1 ak-sticky_header ak-site_header_full_width">
       <div className="header-top">
@@ -178,7 +157,14 @@ function Header({ data }) {
               />
             </Link>
           </div>
-          <button className="ak-menu-toggle" id="akMenuToggle" type="button">
+          <button
+            className="ak-menu-toggle"
+            id="akMenuToggle"
+            type="button"
+            ref={(el) => {
+              akMenuToggle = el;
+            }}
+          >
             <svg
               viewBox="0 0 20 15"
               width="40px"
@@ -191,7 +177,7 @@ function Header({ data }) {
             </svg>
           </button>
 
-          <ul className="top-main-menu">
+          <ul className="top-main-menu" ref={menuRef}>
             {data?.map((item) => (
               <li className="top-main-menu-li">
                 <Link href={item.url}>{item.name}</Link>
@@ -200,11 +186,21 @@ function Header({ data }) {
           </ul>
         </div>
       </div>
-      <div className="nav-bar-border" />
+      <div
+        className="nav-bar-border"
+        ref={(el) => {
+          border1 = el;
+        }}
+      />
       <div className="ak-main_header">
         <div className="container">
           <div className="ak-main_header_in">
-            <div className="ak-main_header_right">
+            <div
+              className="ak-main_header_right"
+              ref={(el) => {
+                hamsdf = el;
+              }}
+            >
               <div className="ak-nav ak-medium">
                 <ul className="ak-nav_list">
                   {data?.map((item) => (
